88f660118dfe1dc504897a5debfeeb07d7a12fc3
huangjia <654664168@qq.com>
2018-07-31 17:41:29 +0800

    add prism packages

--- DevUtils/App.xaml.cs
+++ DevUtils/App.xaml.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using DevUtils.Views;
+using Prism.Autofac;
+using Prism.Ioc;
 
 namespace DevUtils
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
     }
 }
